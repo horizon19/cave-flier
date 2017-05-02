56e093a7645eac635e59619277bc5b82b0886c85
CerberusOne <aingaran2003@gmail.com>
2017-05-01 17:38:57 -0700

    fixing tabbing

--- Cave Flier/Assets/Scripts/playerMovement.cs
+++ Cave Flier/Assets/Scripts/playerMovement.cs
@@ -11,7 +11,7 @@ public class playerMovement : MonoBehaviour
     public float speed = 6;
 
 	// Use this for initialization, called on first frame the object is active
-	void Start ()
+    void Start ()
     {
         Rigidbody rigidbody = GetComponent<Rigidbody>();    //get the physics of the object
         rigidbody.freezeRotation = true;    //stop the object from rotating
