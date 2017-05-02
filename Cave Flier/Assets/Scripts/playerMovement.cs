fb13b9ff19f48e5c739aeecc5269654e877ae8a9
CerberusOne <aingaran2003@gmail.com>
2017-05-01 17:29:00 -0700

    updating PR

--- Cave Flier/Assets/Scripts/playerMovement.cs
+++ Cave Flier/Assets/Scripts/playerMovement.cs
@@ -6,11 +6,13 @@ using System;
 using System.Globalization;
 
 
-public class playerMovement : MonoBehaviour {
+public class playerMovement : MonoBehaviour
+{
     public float speed = 6;
 
 	// Use this for initialization, called on first frame the object is active
-	void Start () {
+	void Start ()
+    {
         Rigidbody rigidbody = GetComponent<Rigidbody>();    //get the physics of the object
         rigidbody.freezeRotation = true;    //stop the object from rotating
     }   
