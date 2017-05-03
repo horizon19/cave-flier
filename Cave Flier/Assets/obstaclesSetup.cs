47e79428ea4510946df835cec113e13a31cfbfe8
CerberusOne <aingaran2003@gmail.com>
2017-05-03 10:18:40 -0700

    removed several obstacles and google cardboard demos

---
+++ Cave Flier/Assets/obstaclesSetup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstaclesSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.material.color = Color.grey;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
