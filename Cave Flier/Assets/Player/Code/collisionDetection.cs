1c2248e78a063a8d3b452e6f98910077b11aa64f
Shirou <Jayscogs@gmail.com>
2017-05-04 11:53:21 -0700

    fixed PR issues

--- Cave Flier/Assets/Player/Code/collisionDetection.cs
+++ Cave Flier/Assets/Player/Code/collisionDetection.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collisionDetection : MonoBehaviour {
+public class collisionDetection : MonoBehaviour 
+{
 
 	// Use this for initialization
-	void Start ()
+    void Start ()
     {
 		
 	}
 	
 	// Update is called once per frame
-	void Update ()
+    void Update ()
     {
 		
-	}
+    }
 
     /**
     * Date:             May 3, 2017
